69446dae5f880497ff3e03290a4954878b075468
Jnaesi <jacobnasi@hotmail.com>
2022-06-13 10:13:51 +0000

    add functions in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incremetWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+    
+}
